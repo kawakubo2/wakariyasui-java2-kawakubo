a1f5d2df003e5af4f16a0493f1e563e888cdde22
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-10-28 07:26:29 +0900

    2022年10月27日分

---
+++ src/chapter11/Pass11_5.java
@@ -0,0 +1,27 @@
+package chapter11;
+
+public class Pass11_5 {
+    public static void main(String[] args) {
+        String[] name = { "田中", "前田", "鈴木", "中村", "田辺", "浦川", "島田", "岩田", "加藤" };
+        int[] drinking = { 7, 0, 3, 3, 2, 0, 0, 6, 3 };
+        int[] smoking = { 60, 10, 0, 20, 10, 0, 30, 0, 30 };
+
+        for (int i = 0; i < name.length; i++) {
+            int d = drinking[i];
+            int s = smoking[i];
+            System.out.printf("%s(%d, %d)\t", name[i], d, s);
+            if (d == 0 && s == 0) {
+                System.out.println("安全");
+            }
+            if ((d == 0 && s >= 1 && s <= 20) || d >= 1 && d <= 3 && s == 0) {
+                System.out.println("注意");
+            }
+            if (d >= 1 && d <= 3 && s >= 1 && s <= 20) {
+                System.out.println("要指導");
+            }
+            if (d >= 4 || s >= 21) {
+                System.out.println("要検査");
+            }
+        }
+    }
+}
