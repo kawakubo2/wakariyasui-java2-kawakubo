6d341c8983b4174b8ea46e34224532011c72110e
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2022-10-29 08:42:21 +0900

    2022年10月28日分

---
+++ src/chapter12/Sample12_3.java
@@ -0,0 +1,15 @@
+package chapter12;
+
+import lib.Input;
+
+public class Sample12_3 {
+    public static void main(String[] args) {
+        int n = Input.getInt();
+        String msg = switch (n) {
+            case 10 -> "正常終了";
+            case 20, 21 -> "エラー発生";
+            default -> "未定義";
+        };
+        System.out.println(msg);
+    }
+}
