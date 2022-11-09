21c9a7f0ebbea5f62a8da6eafff10d700237b523
kawakubo <tomo.kawakubo@gmail.com>
2022-11-09 20:32:39 +0900

    2022年11月09日新マシンでのcommit

--- src/chapter07/Pass7_4.java
+++ src/chapter07/Pass7_4.java
@@ -4,7 +4,6 @@ import lib.Input;
 
 public class Pass7_4 {
     public static void main(String[] args) {
-        Double[] y = { 10.2, null, 12.5, 8.0, 5.5 };
         double[] x = { 10.2, 4.5, 12.5, 8.0, 5.5 };
         double a = Input.getDouble();
         for (int i = 0; i < 5; i++) {
--- src/chapter07/Sample7_1.java
+++ src/chapter07/Sample7_1.java
@@ -2,15 +2,21 @@ package chapter07;
 
 public class Sample7_1 {
     public static void main(String[] args) {
-         int[] n = {3, 5, 10, 14, 28};
-         double[] x = {0.3, 1.05, 2.3};
-         char[] c = {'お', 'は', 'よ', 'う'};
-         String[] s = {"おはよう", "こんにちは", "こんばんは"};
-         boolean[] b = {true, false, true, true};
-        
-         s[0] = "Good Morning";
-         System.out.println(s[0]);
-         System.out.println(s[1]);
-         System.out.println(s[2]);
+        int[] n = { 3, 5, 10, 14, 28 };
+        double[] x = { 0.3, 1.05, 2.3 };
+        char[] c = { 'お', 'は', 'よ', 'う' };
+        String[] s = { "おはよう", "こんにちは", "こんばんは" };
+        boolean[] b = { true, false, true, true };
+
+        s[0] = "Good Morning";
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+        System.out.println(s[2]);
+
+        System.out.println(n);
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(b);
     }
 }
