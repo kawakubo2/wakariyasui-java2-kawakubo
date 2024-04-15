0795a5eace74fae4b1d746bbd8a76be4802e6a6b
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-04-15 21:05:44 +0900

    テスト

--- src/test/MyFileWalker.java
+++ src/test/MyFileWalker.java
@@ -2,19 +2,32 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MyFileWalker {
-    public static void main(String[] args) {
-        var path = Path.of("./src/test/Season.java");
-        try (BufferedReader br = Files.newBufferedReader(path);) {
+    public void replace(String filepath) {
+        var path = Path.of(filepath);
+
+        Path tempPath = Path.of("temp.txt"); 
+        try (BufferedReader in = Files.newBufferedReader(path);
+             PrintWriter out = new PrintWriter(Files.newBufferedWriter(tempPath));) {
+
             String line = "";
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+            int number = 1;
+            while ((line = in.readLine()) != null) {
+                //out.printf("%d %s%n", number++, line);
+                out.println(line);
             }
+            Files.delete(path);
+            Files.move(tempPath, Path.of(filepath));
         } catch (IOException e) {
             System.out.println(e);
         }
+    }
+    public static void main(String[] args) {
+        MyFileWalker mf = new MyFileWalker();
+        mf.replace("src/test/README.md");
     }
 }
---
+++ src/test/README.md
@@ -0,0 +1,18 @@
+1 ## Getting Started
+2 
+3 Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+4 
+5 ## Folder Structure
+6 
+7 The workspace contains two folders by default, where:
+8 
+9 - `src`: the folder to maintain sources
+10 - `lib`: the folder to maintain dependencies
+11 
+12 Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+13 
+14 > If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+15 
+16 ## Dependency Management
+17 
+18 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
